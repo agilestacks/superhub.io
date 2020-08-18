fe59564db9135768e5d12185e1c5e422ace92cb2
Igor Lysak <igorlysak@agilestacks.com>
2020-08-18 18:57:11 +0300

    Publishing to gh-pages

---
+++ js/site.12df8f03537a80a30f9a880109925b72ab22fcedbfda69fca25795779a3ebbc9.js
@@ -0,0 +1,61 @@
+function onCopy(os) {
+    const pre = document.querySelector(`pre#${os}-instruction`);
+    const input = document.querySelector(`textarea#${os}`);
+    input.value = pre.innerHTML.trim();
+    input.select();
+    document.execCommand('copy');
+    input.setSelectionRange(0, 0)
+}
+
+function selectOSTab(id) {
+    document.querySelector(`li#${id}`).classList.add('is-active');
+}
+
+(function () {
+    const {appVersion} = navigator;
+    if (appVersion.indexOf('Win') !== -1) {
+        selectOSTab('windows');
+    }
+    if (appVersion.indexOf('Mac') !== -1) {
+        selectOSTab('macos');
+    }
+    if (appVersion.indexOf('X11') !== -1 || appVersion.indexOf('Linux') !== -1) {
+        selectOSTab('linux');
+    }
+})();
+
+let tabsWithContent = (function () {
+    let tabs = document.querySelectorAll('.tabs li');
+    let tabsContent = document.querySelectorAll('.tab-content');
+
+    let deactvateAllTabs = function () {
+        tabs.forEach(function (tab) {
+            tab.classList.remove('is-active');
+        });
+    };
+
+    let hideTabsContent = function () {
+        tabsContent.forEach(function (tabContent) {
+            tabContent.classList.remove('is-active');
+        });
+    };
+
+    let activateTabsContent = function (tab) {
+        tabsContent[getIndex(tab)].classList.add('is-active');
+    };
+
+    let getIndex = function (el) {
+        return [...el.parentElement.children].indexOf(el);
+    };
+
+    tabs.forEach(function (tab) {
+        tab.addEventListener('click', function () {
+            deactvateAllTabs();
+            hideTabsContent();
+            tab.classList.add('is-active');
+            activateTabsContent(tab);
+        });
+    })
+
+    tabs[0].click();
+})();
